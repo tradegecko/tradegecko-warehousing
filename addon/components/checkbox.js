00b7d37f50c08eed4055933402eeba4035d4098b
selvagsz <selvaganeshbsg@gmail.com>
2018-08-01 19:59:11 +0800

    [Feature] Checkbox component

---
+++ addon/components/checkbox.js
@@ -0,0 +1,33 @@
+import Component from '@ember/component';
+import { PropTypes } from 'ember-prop-types';
+import prefix from '../utils/computed/prefix';
+import layout from '../templates/components/button';
+
+const Checkbox = Component.extend({
+  layout,
+  classNames: ['tradegecko-ui-checkbox'],
+  classNameBindings: [
+    'disabled:tradegecko-ui-checkbox--disabled',
+    'checked:tradegecko-ui-checkbox--checked',
+  ],
+  uniqueID: prefix('elementId', 'tradegecko-ui-checkbox_'),
+
+  change(event) {
+    if (this.onChange && !this.get('disabled')) {
+      this.onChange(event);
+    }
+  },
+});
+
+Checkbox.propTypes = {
+  checked: PropTypes.bool,
+  disabled: PropTypes.bool,
+  onChange: PropTypes.func,
+};
+
+Checkbox.defaultProps = {
+  checked: false,
+  disabled: false,
+};
+
+export default Checkbox;
