f8a9e4e544e2268b4635c4999689f4f8a5f9f41b
selvagsz <selvaganeshbsg@gmail.com>
2018-08-13 16:36:38 +0800

    [Feature] component:list-empty-state

---
+++ addon/components/list-empty-state.js
@@ -0,0 +1,7 @@
+import Component from '@ember/component';
+import layout from '../templates/components/list-empty-state';
+
+export default Component.extend({
+  layout,
+  classNames: ['tradegecko-ui-empty-list'],
+});
